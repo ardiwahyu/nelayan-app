050cd44a9867e67dc6a3b63d4f43b5dd1d5f4f80
Wahyu Ardiansyah <wahyuardi027@gmail.com>
2021-02-15 09:03:35 +0700

    init project

---
+++ app/src/main/java/id/siap/nelayanapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.siap.nelayanapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
