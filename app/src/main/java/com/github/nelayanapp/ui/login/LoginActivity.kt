cf4251b3359489a7357cdb5c481979a382dcbca3
Wahyu Ardiansyah <wahyuardi027@gmail.com>
2021-02-23 01:13:39 +0700

    halaman profil

---
+++ app/src/main/java/com/github/nelayanapp/ui/login/LoginActivity.kt
@@ -0,0 +1,43 @@
+package com.github.nelayanapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.*
+import com.bumptech.glide.request.RequestOptions
+import com.github.nelayanapp.R
+import com.github.nelayanapp.databinding.ActivityLoginBinding
+import com.github.nelayanapp.ui.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.intentFor
+
+@AndroidEntryPoint
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        Glide.with(this)
+            .load(ContextCompat.getDrawable(this, R.mipmap.bg))
+            .centerInside()
+            .transform(MultiTransformation(
+                CenterCrop(),
+                GranularRoundedCorners(0f,0f,100f,100f)
+                )
+            ).into(ivBg)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(intentFor<MainActivity>())
+        }
+    }
+}
--- app/src/main/java/com/github/nelayanapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/nelayanapp/ui/main/MainActivity.kt
@@ -3,15 +3,36 @@ package com.github.nelayanapp.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.github.nelayanapp.R
+import com.github.nelayanapp.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    private val mainViewModel: MainViewModel by viewModels()
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navController = findNavController(R.id.navHostFragment)
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
