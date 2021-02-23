f58f4f42461e23c685bebfbfacf78ac1f1b54174
Wahyu Ardiansyah <wahyuardi027@gmail.com>
2021-02-23 13:30:59 +0700

    activity verifikasi

--- app/src/main/java/com/github/nelayanapp/ui/daftar/DaftarActivity.kt
+++ app/src/main/java/com/github/nelayanapp/ui/daftar/DaftarActivity.kt
@@ -3,10 +3,26 @@ package com.github.nelayanapp.ui.daftar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.github.nelayanapp.R
+import com.github.nelayanapp.databinding.ActivityDaftarBinding
+import com.github.nelayanapp.ui.verifikasi.VerifikasiNomorActivity
+import dagger.hilt.android.AndroidEntryPoint
+import org.jetbrains.anko.intentFor
 
+@AndroidEntryPoint
 class DaftarActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDaftarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_daftar)
+        binding = ActivityDaftarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUi()
+    }
+
+    private fun initUi() {
+        binding.btnLanjut.setOnClickListener {
+            startActivity(intentFor<VerifikasiNomorActivity>())
+        }
     }
 }
---
+++ app/src/main/java/com/github/nelayanapp/ui/verifikasi/VerifikasiNomorActivity.kt
@@ -0,0 +1,12 @@
+package com.github.nelayanapp.ui.verifikasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.nelayanapp.R
+
+class VerifikasiNomorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verifikasi_nomor)
+    }
+}
